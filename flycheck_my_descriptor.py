7e828375d2bd4fa57174fb13b05149e7dca73137
Reuven Lerner <reuven@lerner.co.il>
2023-12-25 11:31:47 +0200

    gitautosync update 44

---
+++ flycheck_my_descriptor.py
@@ -0,0 +1,15 @@
+class MyDescriptor:
+    def __init__(self):
+        print(f'In MyDescriptor.__init__')
+    def __get__(self, instance, owner):
+        print(f'In MyDescriptor.__get__')
+        return 12345
+    def __del__(self):
+        print(f'Now deleting instance of MyDescriptor')
+
+class MyClass:
+    x = MyDescriptor()   # x is a class attribute, its value is instance of MyDescriptor
+
+m = MyClass()
+print(m.x)
+print(m.x * 2)
