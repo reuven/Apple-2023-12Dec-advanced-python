7a52d6eae70bc20b3f9606bb9f2e115c1e8ecd59
Reuven Lerner <reuven@lerner.co.il>
2023-12-25 12:17:24 +0200

    gitautosync update 77

--- ipython_log.py
+++ ipython_log.py
@@ -171,3 +171,35 @@ for one_item in c:
 print('** second time **')
 for one_item in c:
     print(one_item)
+class CircleIterator:
+    def __init__(self, data, maxtimes):
+        self.data = data
+        self.maxtimes = maxtimes
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= self.maxtimes:
+            raise StopIteration
+
+        value = self.data[self.index % len(self.data)]
+        self.index += 1
+        return value
+    
+
+class Circle:
+    def __init__(self, data, maxtimes):
+        self.data = data
+        self.maxtimes = maxtimes
+
+    def __iter__(self):
+        return CircleIterator(self.data, self.maxtimes)
+
+c = Circle('abcd', 5)
+
+print('** first time **')
+for one_item in c:
+    print(one_item)
+
+print('** second time **')
+for one_item in c:
+    print(one_item)
