388df5f371fcc9685c968199c86f49d897e74e2d
Reuven Lerner <reuven@lerner.co.il>
2023-12-25 11:30:41 +0200

    gitautosync update 41

--- ipython_log.py
+++ ipython_log.py
@@ -3,3 +3,19 @@
 
 import gc
 gc.get_referrers(MyDescriptor)
+class MyDescriptor:
+    def __init__(self):
+        print(f'In MyDescriptor.__init__')
+    def __get__(self, instance, owner):
+        print(f'In MyDescriptor.__get__')
+        return 12345
+    def __del__(self):
+        print(f'Now deleting instance of MyDescriptor')
+
+class MyClass:
+    x = MyDescriptor()   # x is a class attribute, its value is instance of MyDescriptor
+
+m = MyClass()
+print(m.x)
+print(m.x * 2)
+gc.get_referrers(MyDescriptor)
