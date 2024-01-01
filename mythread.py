4710e124edc9ee6e79eeffa502a9c23c78898303
Reuven Lerner <reuven@lerner.co.il>
2024-01-01 11:44:44 +0200

    gitautosync update 96

--- mythread.py
+++ mythread.py
@@ -8,12 +8,14 @@ def hello(n):
     time.sleep(random.randint(0, 3))
     print(f'{n} Hello!')
 
+all_threads = []
 for i in range(10):
     t = threading.Thread(target=hello, args=(i,))
+    all_threads.append(t)
     t.start()
 
 # "join" on a thread means: wait until it's done
-
-
+for one_thread in all_threads:
+    t.join()
 
 print('Done!')
