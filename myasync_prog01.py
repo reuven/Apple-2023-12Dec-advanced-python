08818b6c9104bedf263237d90ac4df4e97931b0d
Reuven Lerner <reuven@lerner.co.il>
2024-01-03 10:52:41 +0200

    gitautosync update 79

--- myasync_prog01.py
+++ myasync_prog01.py
@@ -2,7 +2,11 @@
 
 import asyncio
 
+# define our function with "async def"
 async def hello(n):
     print(f'{n} Hello!')
+
+# run the async def, and get a coroutine back
+# schedule the coroutine with asyncio.run
 
 asyncio.run(hello(5))
