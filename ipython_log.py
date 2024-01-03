8ddba96782c7f449ebf8cb2296ddae9908b71c4f
Reuven Lerner <reuven@lerner.co.il>
2024-01-03 12:08:13 +0200

    gitautosync update 148

--- ipython_log.py
+++ ipython_log.py
@@ -124,3 +124,5 @@ min_temps = np.array([13, 12, 12, 11, 11, 11, 10, 12, 10, 8])
 max_temps = np.array([19, 20, 18, 22, 24, 22, 19, 18, 16, 15])
 max_temps.mean()
 max_temps[:3]
+max_temps[:3].mean()
+max_temps[range(3)].mean()
