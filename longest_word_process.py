fea222458546548d63bfa00ab05d5ed520f80c43
Reuven Lerner <reuven@lerner.co.il>
2024-01-03 10:25:38 +0200

    gitautosync update 60

---
+++ longest_word_process.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ProcessPoolExecutor
+import glob
+import time
+
+def longest_word(filename):
+    output = ''
+
+    try:
+        for one_line in open(filename):
+            for one_word in one_line.split():
+                if len(one_word) > len(output):
+                    output = one_word
+    except Exception as e:
+        return filename, f'[Error reading from file {filename}]'
+
+    return filename, output
+
+start_time = time.time()
+with ProcessPoolExecutor(max_workers=1) as executor:
+    results = executor.map(longest_word,
+                           glob.glob('/etc/*.conf'))
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f'Total time: {total_time}')
+    for filename, one_result in results:
+        print(f'{filename}: {one_result.strip()}')
