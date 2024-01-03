c965a436856d56d5069dc81ca5c3d1c3b921bbc6
Reuven Lerner <reuven@lerner.co.il>
2024-01-03 09:51:13 +0200

    gitautosync update 21

--- mythread5.py
+++ mythread5.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
 from concurrent.futures import ThreadPoolExecutor, wait
+
+def count_vowels(s):
+    total = 0
+
+    for one_character in s:
+        if one_character in 'aeiou':
+            total += 1
+
+    return total
+
+words = 'this is a fantastic and enticing and superfabulous sentence'.split()
+
+with ThreadPoolExecutor() as executor:
+    results = executor.map(count_vowels, words)
+
+    print(results)
