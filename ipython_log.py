d5cbd0222a0c2c8ca8fd3e226cef962bacfcf9d3
Reuven Lerner <reuven@lerner.co.il>
2024-01-03 09:45:12 +0200

    gitautosync update 11

--- ipython_log.py
+++ ipython_log.py
@@ -12,3 +12,12 @@ numbers = range(10)
 [one_number ** 2
  for one_number in numbers
  if one_number % 2]
+# how does map work?
+
+# we pass map two arguments:
+# 1. a function that takes one argument
+# 2. an iterable of data
+
+words = 'this is a bunch of words'
+
+map(len, words)
