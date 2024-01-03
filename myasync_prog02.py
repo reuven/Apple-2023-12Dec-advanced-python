1937df8537249c4a522f80fcbb30532ed391a4bd
Reuven Lerner <reuven@lerner.co.il>
2024-01-03 10:55:06 +0200

    gitautosync update 86

--- myasync_prog02.py
+++ myasync_prog02.py
@@ -6,7 +6,10 @@ import asyncio
 async def hello(n):
     print(f'{n} Hello!')
 
+async def main():
+    for i in range(5):
+        hello(i)
+
 # run the async def, and get a coroutine back
 # schedule the coroutine with asyncio.run
-for i in range(5):
-    asyncio.run(hello(i))
+asyncio.run(main())
